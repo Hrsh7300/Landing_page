c847f9b8b391d4acaff54088c5ba435948a1c911
Harsh Pareek <114802400+Hrsh7300@users.noreply.github.com>
2024-06-02 11:52:52 +0530

    Add files via upload

---
+++ landingpage/src/constants/menuItems.ts
@@ -0,0 +1,20 @@
+const menuItems = [
+  {
+    value: "Home",
+    id: "1"
+  },
+  {
+    value: "About Us",
+    id: "2"
+  },
+  {
+    value: "Pricing",
+    id: "3"
+  },
+  {
+    value: "Features",
+    id: "4"
+  },
+];
+
+export default menuItems;
